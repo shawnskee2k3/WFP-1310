9082d71e663b1a5369cf6e44eca86ec1d0b6b7c5
Solomon Amos <shawnskee2k3@fullsail.edu>
2013-10-31 22:12:25 -0400

    Home page and files
    This is waht the home page code from wordpress looks like.
    Just for the main page.

---
+++ Card Fight Anime | A site like no other_files/custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+			jQuery('.bxslider').bxSlider( {
+			mode: 'horizontal',
+			captions: true,
+			minSlides: 1,
+			maxSlides: 1,
+			adaptiveHeight: true,
+			slideWidth: 1090,
+			auto: true,
+			pause: 5000,
+			autoHover: true } );
+			});
